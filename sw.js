150a2a0986b5a2b9a447d4c62575d5bf5ce4f0f1
Eduardo Vela <eduardovelarios@gmail.com>
2021-11-27 01:24:25 -0600

    Primer commit

---
+++ sw.js
@@ -0,0 +1,81 @@
+const CACHE ='cache-1';
+const CACHE_DINAMICO ='dinamico-1';
+const CACHE_INMUTABLE ='inmutable-1';
+
+
+self.addEventListener('install', evento =>{
+    const promesa =caches.open(CACHE)
+    .then(cache=>{
+         return cache.addAll([
+             "/",
+             "/index.html",
+             "css/style.css",
+             "img/CWBE.png",
+             "img/no-img.jpeg",
+             "js/app.js",
+             "pages/offline.html"
+
+             
+            ]);
+        });
+        
+
+evento.waitUntil(Promise.all([promesa ]));
+});
+
+self.addEventListener('activate', evento =>{
+    const respuesta =caches.keys().then(keys =>{
+        keys.forEach(key =>{
+            if(key !== CACHE && key.includes("cache")){
+                return caches.delete(key);
+        }
+    });
+});
+evento.waitUntil(respuesta);
+
+});
+
+
+
+self.addEventListener('fetch', evento =>{
+
+//2
+const respuesta=caches.match(evento.request) 
+    .then(res=>{
+        if (res) return res;
+        console.log('No existe', evento.request.url);
+         return fetch(evento.request)
+          .then(resWeb=>{
+              caches.open(CACHE_DINAMICO)
+              .then(cache=>{
+                  cache.put(evento.request,resWeb);
+                  limpiarCache(CACHE_DINAMICO,50);
+                 })
+                 return resWeb.clone(); 
+                });
+            })
+                .catch(err => {
+                    if(evento.request.headers.get('accept').includes('text/html')){
+                        return caches.match('/pages/offline.html');
+                    }else{
+                        return caches.matchç('img/no-img.jpeg');
+                    }
+
+                 });
+     evento.respondWith(respuesta);
+    });
+
+
+   
+function limpiarCache(nombreCache, numeroItems){
+     caches.open(nombreCache)
+     .then(cache=>{
+         return cache.keys()
+          .then(keys=>{
+               if (keys.length>numeroItems){
+                   cache.delete(keys[0])
+                   .then(limpiarCache(nombreCache, numeroItems));
+                 }
+                });
+            });
+        }
