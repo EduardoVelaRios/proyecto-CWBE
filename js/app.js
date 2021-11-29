2f4f42b8770a1376acb8c9a79659c17f23387fcd
Eduardo Vela <eduardovelarios@gmail.com>
2021-11-28 19:41:37 -0600

    Segundo Commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
 if ( navigator.serviceWorker ) {
     navigator.serviceWorker.register('/sw.js');
    }
-   var url=window.location.href;
-   var ubicacionSw='/CWBE/sw.js';
-
-   if ( navigator.serviceWorker ) {
-    if(url.includes('localhost')){
-    ubicacionSw='/sw.js';
-    }
-    navigator.serviceWorker.register(ubicacionSw);
-   }        
+ 
